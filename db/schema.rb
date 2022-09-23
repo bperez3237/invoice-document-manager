fcb274d335b510d7daaa8fc646aa39467f2d0ce3
Brian <b.perez3237@gmail.com>
2022-09-23 18:50:21 -0400

    Setup client side

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2022_09_23_215218) do
 
   create_table "subcontractors", force: :cascade do |t|
     t.string "name"
-    t.string "period_end"
+    t.date "period_end"
     t.integer "lunch"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
