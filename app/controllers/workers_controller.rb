963d73dc7345f1b514a083543a7b592b1e5fd6f1
Brian <b.perez3237@gmail.com>
2022-09-23 18:34:26 -0400

    Write worker controller methods

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,2 +1,45 @@
 class WorkersController < ApplicationController
+
+    def show
+        worker = Worker.find(params[:id])
+        if worker
+            render json: worker
+        else 
+            render json: { error: "Worker Not Found"}, status: :not_found
+        end
+    end
+
+    def index
+        workers = Worker.all
+        render json: workers
+    end
+
+    def create
+        worker = Worker.create(worker_params)
+        if worker.valid?
+            render json: worker, status: :created
+        else
+            render json: {error: worker.errors}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        worker = Worker.find(params[:id])
+        if worker
+            worker.update(worker_params)
+            if worker.valid?
+                render json: worker
+            else 
+                render json: {error: worker.errors}, status: :unprocessable_entity
+            end
+        else
+            render json: {error: "Worker not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def worker_params
+        params.permit(:name, :base, :pt_base, :fringe, :pf_fringe, :section3, :subcontractor_id)
+    end
 end
