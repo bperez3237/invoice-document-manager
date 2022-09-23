46665e373a37601a8674685d4e90cf985baaf166
Brian <b.perez3237@gmail.com>
2022-09-23 18:36:53 -0400

    Write subcontractor controller methods

--- app/controllers/subcontractors_controller.rb
+++ app/controllers/subcontractors_controller.rb
@@ -1,2 +1,16 @@
 class SubcontractorsController < ApplicationController
+
+    def show
+        subcontractor = Subcontractor.find(params[:id])
+        if subcontractor
+            render json: subcontractor
+        else
+            render json: {error: "Subcontractor not found"}, status: :not_found
+        end
+    end
+
+    def index
+        subcontractors = Subcontractor.all
+        render json: subcontractors
+    end
 end
