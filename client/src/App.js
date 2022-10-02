1d629353867cf167f46c503c8b6fecdb6ec258a5
Brian <b.perez3237@gmail.com>
2022-10-02 19:21:20 -0400

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './components/home/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <div className="App">
-        <h1>h1</h1>
+        <h1>Payroll Spreadsheet Generator</h1>
       </div>
       <Switch>
         <Route path="/view-employees">
